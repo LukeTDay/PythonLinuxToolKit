6a0bcce63315fd3e91901eef5a6becd9f5cff0fe
LukeTday <luke.day@gmail.com>
2025-11-09 21:37:38 -0500

    Added a default value for clear_console() so that the duration could be instant if needed

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,6 @@
 import os, time
 
-def clear_console():
+def clear_console(duration: float=0.5):
     """"Clears the console, differs based on the operating system for cross platform integration"""
-    time.sleep(0.5)
+    time.sleep(duration)
     os.system('clear' if os.name == 'posix' else 'cls') 
